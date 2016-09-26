52f3a178e3ce933e4b650ae12894c64c73d1709c
Krystyna Malewski <krmalewski@gmail.com>
2016-09-26 09:39:27 -0400

    Morning exercise with es5

--- w02/d01/morning/starter.js
+++ w02/d01/morning/starter.js
@@ -2,88 +2,82 @@
 //part 1:
 
 //1a
-function kanyesGoldenUnicorn(){console.log('wanted a plantinum unicorn');}
+function kanyesGoldenUnicorn() {
+  console.log('wanted a plantinum unicorn');
+}
 
 //1b
-var items = new Array();
+var items = [];
 
 //1c
 
-var big-animal-array = [
-  "elephant"
-  , "tiger"
-  , "bunny"
-  , "unicorn"
-  , "polar bear"
-]
-
+var bigAnimalArray = ['elephant',
+'tiger',
+'bunny',
+'unicorn',
+'polar bear'
+];
 
 
 //part 2:
 
-var pickle = 22
+var pickle = 22;
+var cat = Math.random();
 
-if(9 < var cat = Math.random()){console.log('Found the cat!')};;
-else if (pickle < 4){ console.log('AAHRHW')}
+if (9 < cat) {
+  console.log('Found the cat!');
+} else if (pickle < 4) {
+  console.log('AAHRHW');
+};
 
 
 //part 3:
-
-
-  function login() {
-
-if (response.status === 'connected') {
-document.getElementById('status').innerHTML = "we are con"
-
-}
-
-else if (response.status === 'not_authorized') {
-  document.getElementById('status').innerHTML = "Not connected";
-   hideContainer();
-
-}
-else {
-  document.getElementById('status').innerHTML = "You are not logged in into facebook, please log in to take a quiz";
+function login() {
+  if (response.status === 'connected') {
+    document.getElementById('status').innerHTML = 'we are con';
+  } else if (response.status === 'not_authorized') {
+    document.getElementById('status').innerHTML = 'Not connected';
+    hideContainer();
+  } else {
+    document.getElementById('status').innerHTML = 'You are not logged in into facebook, please log in to take a quiz';
+  };
 };
-}
 
 
 
 //part 4:
-function howMany(selectObject) {var numberSelected = 0
-for (var i = 0, i < selectObject.options.length, i++) {if
-  (selectObject.options[i].selected) {
+function howMany(selectObject) {
+  var numberSelected = 0;
+  for (var i = 0, i < selectObject.options.length, i++) {
+    if (selectObject.options[i]) {
       numberSelected++;
-  }
-  }
-return numberSelected;
-}
+    };
+  };
+  return numberSelected;
+};
 
 
 //part 5:
-      let userInfo = {fname: req.body.fname,
-        lname: req.body.lname,
-        email: email,
-        passwordDigest: hash
-    }
+let userInfo = {
+  'fname': req.body.fname,
+  'lname': req.body.lname,
+  'email': email,
+  'passwordDigest': hash
+};
 
 
 
 
 //part 6:
-if( $('#check').html().length +1  === $('#userPick').html().length ) {
-       $winnermsg.append('Congratulation you won')
-      newValue ++;
-       $(".balance").text(newValue);
+if ($('#check').html().length + 1  === $('#userPick').html().length ) {
+  $winnermsg.append('Congratulation you won');
+  newValue ++;
+  $('.balance').text(newValue);
+} else {
+  newValue -= 1;
+  $('.balance').text(newValue);
+};
 
-  }
-     else {
-       newValue -=1;
-       $(".balance").text(newValue);
-
-     };
-
-}
 
 //
 
