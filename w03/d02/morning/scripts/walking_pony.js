772824bf34bb9eccb30c09581fbf86dfb75a4bde
Krystyna Malewski <krmalewski@gmail.com>
2016-10-05 09:00:18 -0400

    ...

--- w03/d02/morning/scripts/walking_pony.js
+++ w03/d02/morning/scripts/walking_pony.js
@@ -1,1 +1,44 @@
-console.log("PONY POWER!!");
+// console.log("PONY POWER!!");
+
+// function walkingPony() {
+//   $('#pony').css("marginLeft", '1500px');
+//   $('#pony').animate({
+//     marginLeft: '-500px'
+//   }, 5000, walkingPony)
+// }
+
+
+// setInterval(walkingPony, 100)
+
+
+function trot() {
+  var position = $('#pony').offset().left;
+  // Once pony goes off the page, reset it to the right side of the page
+  if (position < -500) {
+    position = 1280;
+  }
+  $('#pony').css('margin-left', position-20)
+// }
+}
+
+setInterval(trot, 100);
+
+
+
+
+// function walkingPony() {
+//  var $pony = $('#pony');
+//  var distance = 1200;
+//  setInterval(function() {
+//    $pony.css('left', distance + 'px');
+//    if (distance < -300) {
+//      distance = 1200;
+//    } else {
+//      distance -= 5;
+//    }
+//  }, 10);
+// };
+
+// $(document).ready(function(){
+//  walkingPony();
+// })
