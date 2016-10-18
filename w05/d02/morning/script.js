695e27c3f29b0c6387c3be5b9afeb96c388dbf37
Krystyna Malewski <krmalewski@gmail.com>
2016-10-18 10:35:59 -0400

    Add comments

--- w05/d02/morning/script.js
+++ w05/d02/morning/script.js
@@ -29,21 +29,22 @@ var frameworks = {
   ] // ends backendArray
 } // ends data json object
 
-
-frameworks.frontEnd.forEach(function(object) {
+// Console.log the name and description of each frontend framework using forEach
+frameworks.frontEnd.forEach( object => {
   console.log(object.name + ' is a ' + object.description);
 });
 
+// Find the sum of an array using forEach
 const arr = [3, 5, 2, 8, 1];
 
 let sum = 0;
-arr.forEach(function(n) {
+arr.forEach( n => {
   sum += n;
 });
 console.log(sum);
 
-
-const modifiedFrontEnd = frameworks.frontEnd.map(function(object) {
+// Use map to create a new array with .js at the end of the names
+const modifiedFrontEnd = frameworks.frontEnd.map( object => {
   return {
     'name': object.name + '.js',
     'description': object.description,
@@ -52,7 +53,7 @@ const modifiedFrontEnd = frameworks.frontEnd.map(function(object) {
 
 console.log(modifiedFrontEnd);
 
-const modifiedBackEnd = frameworks.backEnd.map(function(object) {
+const modifiedBackEnd = frameworks.backEnd.map( object => {
   return {
     'name': object.name + '.js',
     'description': object.description,
@@ -61,8 +62,8 @@ const modifiedBackEnd = frameworks.backEnd.map(function(object) {
 
 console.log(modifiedBackEnd);
 
-
-const frontEndFilter = frameworks.frontEnd.filter(function(object) {
+// Retrun a new array of objects that contains only frameworks with a description longer than 30 characters
+const frontEndFilter = frameworks.frontEnd.filter( object => {
   if (object.description.length > 30) {
     return object;
   }
@@ -70,7 +71,8 @@ const frontEndFilter = frameworks.frontEnd.filter(function(object) {
 
 console.log(frontEndFilter);
 
-const backEndFilter = frameworks.backEnd.filter(function(object) {
+// Return a new array of objects that contains only framework's that names begin with a vowel
+const backEndFilter = frameworks.backEnd.filter( object => {
   if (object.name.charAt(0).toUpperCase() === 'A' || object.name.charAt(0).toUpperCase() === 'E' || object.name.charAt(0).toUpperCase() === 'I' || object.name.charAt(0).toUpperCase() === 'O' || object.name.charAt(0).toUpperCase() === 'U') {
     return object;
   }
@@ -78,10 +80,13 @@ const backEndFilter = frameworks.backEnd.filter(function(object) {
 
 console.log(backEndFilter);
 
-
-var frontEndNames = frameworks.frontEnd.reduce(function(str, object) {
+// Return a string that concatnates all the names of the frontend frameworks together
+// If we do not add the extra parameter of an empty string at the end, the first time we loop through it will find the framework object, not object.name
+// By setting the initial value as an empty string, it will loop through and find an empty string the first time and then the second time it will
+// find object.name of the first object
+const frontEndNames = frameworks.frontEnd.reduce( (str, object) => {
   return str += object.name;
-} '')
+},'');
 
 console.log(frontEndNames);
 
