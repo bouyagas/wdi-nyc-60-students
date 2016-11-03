efc3b063358119e921e7f56223b6630a0ccf44be
Krystyna Malewski <krmalewski@gmail.com>
2016-11-03 08:58:58 -0400

    ..

--- w06/d02/morning/starter.js
+++ w06/d02/morning/starter.js
@@ -2,5 +2,87 @@ console.log('starter.js connected');
 
 // Part 1 - Arenagrams
 const areAnagrams = (wordOne, wordTwo) => {
-
+  if (wordOne.length !== wordTwo.length) {
+    return false;
+  }
+  const arr1 = [];
+  const arr2 = [];
+  for (let i = 0; i < wordOne.length; i++) {
+    arr1.push(wordOne.charAt(i));
+  }
+  for ( let j = 0; j < wordTwo.length; j++) {
+    arr2.push(wordTwo.charAt(j));
+  }
+  let x = 0;
+  for ( let k = 0; k < arr1.length; k++) {
+    if (arr2.includes(arr1[k])) {
+      x += 1;
+    }
+  }
+  if ( x === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// Another solution to areAnagrams
+const anargrams = (wordOne, wordTwo) => {
+  let one = wordOne.toLowerCase.split('').sort().join('');
+  let two = wordTwo.toLowerCase.split('').sort().join('');
+  if (one === two) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// Squeeze
+const squeeze = (string) => {
+  let newWord = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) !== string.charAt(i+1)) {
+      newWord += string.charAt(i);
+    }
+  }
+  return newWord;
+}
+
+// Replace Char
+const replaceChar = (string, char, replacementChar) => {
+  let replacedWord = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === char) {
+      replacedWord += replacementChar;
+    } else {
+      replacedWord += string.charAt(i);
+    }
+  }
+  return replacedWord;
+}
+
+
+// StripRight
+let stripRight = (string) => {
+  let word = string;
+  console.log(word)
+  for (let i = string.length + 1; i >= 0; i -1 ) {
+    if (string.charAt(i) === ' ') {
+      word -= string.charAt(i);
+    } else {
+      return word;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
