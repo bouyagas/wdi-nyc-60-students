e7d74d1e3fa44008fb817a65885f180a64b3f1f6
Krystyna Malewski <krmalewski@gmail.com>
2016-11-07 09:33:41 -0500

    Complete operation board game

--- w08/d01/morning/server.js
+++ w08/d01/morning/server.js
@@ -1,1 +1,19 @@
 //SET UP EXPRESS SERVER IN HERE!
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(logger('dev'));
+
+app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => console.log('Server is listening on port ', PORT));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
