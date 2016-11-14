f9f97fcd16e99c092759da7be9550c921a7baa15
Krystyna Malewski <krmalewski@gmail.com>
2016-11-14 10:00:25 -0500

    ticking clock

--- w09/d01/morning/clock.js
+++ w09/d01/morning/clock.js
@@ -1,4 +1,28 @@
 const clock = () => {
+  const minuteHand = document.querySelector('#minute-hand');
+  const secondHand = document.querySelector('#second-hand');
+  const hourHand = document.querySelector('#hour-hand');
+
+  let secondDeg = 0;
+  let minuteDeg = 0;
+  let hourDeg = 0;
+
+  setInterval(function() {
+    secondDeg += 6;
+    secondHand.style.transform=`rotate(${secondDeg}deg)`
+  }, 1000);
+
+  setInterval(function() {
+    minuteDeg += 6;
+    console.log(`minute=${minuteDeg}`);
+    minuteHand.style.transform=`rotate(${minuteDeg}deg)`
+  }, 60000);
+
+  setInterval(function() {
+    hourDeg += 0.5;
+    console.log(`hour=${hourDeg}`);
+    hourHand.style.transform=`rotate(${hourDeg}deg)`
+  }, 60000);
 
 };
 
