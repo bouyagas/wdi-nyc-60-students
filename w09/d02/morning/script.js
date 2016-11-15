25a2ceb62a4c086d5813b3e6a9592502182533ee
Krystyna Malewski <krmalewski@gmail.com>
2016-11-15 10:55:19 -0500

    morning exercise

--- w09/d02/morning/script.js
+++ w09/d02/morning/script.js
@@ -1,19 +1,45 @@
-const slideMenu = () => {
-  // Toggles the `open` class on the content wrapper
-  // and menu wrapper when the hamburger is clicked
+const contentWrapper = document.getElementById('content-wrapper');
+const hamburger = document.getElementById('hamburger-button');
+const menu = document.getElementById('menu');
+// const listItems = menu.childNodes;
+// console.log(listItems);
 
+const slideMenu = () => {
+  hamburger.addEventListener('click', (event) => {
+    if (contentWrapper.className !== 'open') {
+      contentWrapper.setAttribute('class', 'open');
+    } else {
+      contentWrapper.setAttribute('class', '');
+    }
+  });
+// Toggles the `open` class on the content wrapper
+// and menu wrapper when the hamburger is clicked
 };
+slideMenu();
 
 const animateHamburger = () => {
+
   // Toggles the `open` class on the hamburger when clicked
 
 };
 
 const selectMenuItem = () => {
+  let nodes = document.getElementById('menu').childNodes;
+  for(let i = 0; i < nodes.length; i++) {
+    if (nodes[i].nodeName === 'LI') {
+      nodes[i].addEventListener('click', (event) => {
+        if (nodes[i].className !== 'selected') {
+          nodes[i].setAttribute('class', 'selected');
+        } else {
+          nodes[i].setAttribute('class', '');
+        }
+      })
+     }
+  }
+}
   // Adds `selected` class to clicked menu li element and
   // removes `selected` class from all other li elements in the menu *HINT* try using .siblings()
-
-};
+selectMenuItem();
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Script.js linked');
